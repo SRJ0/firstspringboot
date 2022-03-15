cf7fd262cd3599c5fd7a8d1f36f76df54df73fa6
SRJ0 <slche25@gmail.com>
2022-03-15 10:07:07 +0900

    feat: 글 수정 기능 추가

--- src/main/java/com/abc/firstspringboot/web/IndexController.java
+++ src/main/java/com/abc/firstspringboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.abc.firstspringboot.web;
 
 import com.abc.firstspringboot.service.posts.PostsService;
+import com.abc.firstspringboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,4 +25,14 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
 }
